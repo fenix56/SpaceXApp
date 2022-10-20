0565b2e215d0010564c904d53a780568413ae937
Przemek <przemyslaw.falkowski@informationtechconsultants.co.uk>
2022-10-20 17:58:59 +0100

    init commit

---
+++ SpaceXAppUITests/SpaceXAppUITestsLaunchTests.swift
@@ -0,0 +1,21 @@
+//
+//  SpaceXAppUITestsLaunchTests.swift
+//  SpaceXAppUITests
+//
+//  Created by Przemek on 29/08/22.
+//
+
+import XCTest
+
+class SpaceXAppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    
+}
